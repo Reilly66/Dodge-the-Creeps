02a253a9c531a8a9499809ee5a2aad9c121f62c6
Reilly66 <reillyhowlett@icloud.com>
2023-10-19 17:38:38 +0100

    attempt 1 at debugging

---
+++ main/Main.cs
@@ -0,0 +1,77 @@
+using Godot;
+using System;
+
+public partial class Main : Node2D
+{
+
+	[Export]
+	public PackedScene MobScene {get; set; }
+	
+	private int _score;
+
+	private static float GetDirectionToPlayer(Vector2 MobPosition, Vector2 PlayerPosition) {
+		Vector2 vecToPlayer = (PlayerPosition - MobPosition).Normalized();
+		if (vecToPlayer.Length() == 0) return 0;
+		return Mathf.Atan2(vecToPlayer.Y, vecToPlayer.X);
+	}
+
+	public void GameOver() {
+		GetNode<Timer>("MobTimer").Stop();
+		GetNode<Timer>("ScoreTimer").Stop();
+	}
+
+	public void NewGame() {
+		_score = 0;
+		var player = GetNode<Player>("Player");
+		var startPosition = GetNode<Marker2D>("StartPosition");
+		player.Start(startPosition.Position);
+
+		GetNode<Timer>("StartTimer").Start();
+	}
+
+	public void OnScoreTimerTimeout() {
+		_score ++;
+	}
+
+	public void OnStartTimerTimeout() {
+		GD.Print("Start");
+		GetNode<Timer>("MobTimer").Start();
+		GetNode<Timer>("ScoreTimer").Start();
+	}
+
+	public void OnMobTimerTimeout() {
+		GD.Print("mob");
+		// create a new mob
+		Mob mob = MobScene.Instantiate<Mob>();
+		
+		// choose a random location on the path
+		// a random proportion of the total path
+		PathFollow2D mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		// get position of spawn location
+		Vector2 mobSpawnPosition = mobSpawnLocation.Position;
+		// get the angle from the mob spawn position to the player
+		float direction = GetDirectionToPlayer(mobSpawnPosition, GetNode<Player>("Player").Position);
+		// add some randomness to the direction
+		direction += (float)GD.RandRange(-Mathf.Pi/6, Mathf.Pi/6);
+		
+		// set mob velocity
+		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+    	mob.LinearVelocity = velocity.Rotated(direction);
+
+		// spawn the mob
+		AddChild(mob);
+	}
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		NewGame();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
--- player/Player.cs
+++ player/Player.cs
@@ -1,44 +1,81 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 public partial class Player : Area2D
 {
+	// signal to detect collisions
+	[Signal]
+	public delegate void HitEventHandler();
 
 	[Export]
 	public int Speed {get; set; } = 400; // pixels per second for player
+
 	public Vector2 ScreenSize;
+
+	private Vector2 DetectInput() {
+		var direction = Vector2.Zero;
+		if (Input.IsActionPressed("move_right")) direction.X += 1;
+		if (Input.IsActionPressed("move_left")) direction.X -= 1;
+		if (Input.IsActionPressed("move_up")) direction.Y -= 1;
+		if (Input.IsActionPressed("move_down")) direction.Y += 1;
+		// ensure that the magnitude of the direction vector is 1
+		// thus normalising diagonal motion
+		return direction.Normalized();
+	}
+
+	private void AnimatePlayer(Vector2 velocity) {
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		if (velocity.Length()>0) {
+			animatedSprite2D.Play();
+
+			if (velocity.X != 0) {
+				animatedSprite2D.Animation = "walk";
+				animatedSprite2D.FlipV = false;
+				animatedSprite2D.FlipH = velocity.X < 0;
+			} else if (velocity.Y != 0) {
+				animatedSprite2D.Animation = "up";
+				animatedSprite2D.FlipV = velocity.Y > 0;
+			}
+		} else {
+			animatedSprite2D.Stop();
+		}
+	}
+
+	private void MovePlayer(Vector2 velocity, float delta) {
+		Position += velocity * delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+	}
+
+	private void OnBodyEntered(PhysicsBody2D body)
+	{
+		Hide(); // Player disappears after being hit.
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	}
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		ScreenSize = GetViewportRect().Size;
+		Hide();
+	}
+
+	public void Start(Vector2 position) {
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		var velocity = Vector2.Zero;
-
-		if (Input.IsActionPressed("move_right")) velocity.X += 1;
-		if (Input.IsActionPressed("move_left")) velocity.X -= 1;
-		if (Input.IsActionPressed("move_up")) velocity.Y -= 1;
-		if (Input.IsActionPressed("move_down")) velocity.Y += 1;
-
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
-		if (velocity.Length()>0) {
-			// if the magnitude of the velocity vector is greater than 0 a button is pressed
-			velocity = velocity.Normalized() * Speed;
-			// ensure that the magnitude fo the velocity vector is always equal to speed
-			// thus ensuring that diagonal motion is the same speed as unidimensional motion
-			animatedSprite2D.Play();
-		} else {
-			animatedSprite2D.Stop();
-		}
-		Position += velocity * (float) delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
+		var velocity = DetectInput() * Speed;
+		AnimatePlayer(velocity);
+		MovePlayer(velocity, (float) delta);
 	}
 }
