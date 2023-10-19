df614edbfec90438dd0a718d879d4c68c4b44da4
Reilly66 <reillyhowlett@icloud.com>
2023-10-19 19:06:04 +0100

    it works finally
    just gotta make it so that when you start a new game, enemies from the previous game are destroyed

--- main/Main.cs
+++ main/Main.cs
@@ -18,19 +18,26 @@ public partial class Main : Node2D
 	public void GameOver() {
 		GetNode<Timer>("MobTimer").Stop();
 		GetNode<Timer>("ScoreTimer").Stop();
+		GetNode<HUD>("HUD").ShowGameOver();
 	}
 
 	public void NewGame() {
 		_score = 0;
+		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
 		var player = GetNode<Player>("Player");
 		var startPosition = GetNode<Marker2D>("StartPosition");
 		player.Start(startPosition.Position);
-
 		GetNode<Timer>("StartTimer").Start();
+
+		// update score display and show get ready
+		var hud = GetNode<HUD>("HUD");
+		hud.UpdateScore(_score);
+		hud.ShowMessage("Get Ready!");
 	}
 
 	public void OnScoreTimerTimeout() {
 		_score ++;
+		GetNode<HUD>("HUD").UpdateScore(_score);
 	}
 
 	public void OnStartTimerTimeout() {
@@ -68,7 +75,6 @@ public partial class Main : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		NewGame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
